42ccb417c4e81ea9388da40dd457d796360effb2
Bandaru98 <bandarumadhavi681998@gmail.com>
2024-09-11 13:46:18 +0530

    array

---
+++ array.js
@@ -0,0 +1,174 @@
+// store multiple values in a single array - starts with an index vales starts with 0 1 2 .....
+
+//sort
+let array = ['M', 'K', 'A', 'S', 'B']
+array.sort()
+console.log(array);//[ 'A', 'B', 'K', 'M', 'S' ]
+
+//push
+let pusharray = ['M', 'K', 'A', 'S', 'B']
+pusharray.push('madhu')
+console.log(pusharray);//[ 'M', 'K', 'A', 'S', 'B', 'madhu' ]
+
+//reverse
+let revarr = ['M', 'K', 'A', 'S', 'B']
+revarr.reverse()
+console.log(revarr);//[ 'B', 'S', 'A', 'K', 'M' ]
+
+//pop
+let poparr = ['M', 'K', 'A', 'S', 'B']
+poparr.pop()
+console.log(poparr);//[ 'M', 'K', 'A', 'S' ]
+
+//push
+let pusharr = ['M', 'K', 'A', 'S', 'B']
+pusharr.push('keer')
+console.log(pusharr);//[ 'M', 'K', 'A', 'S', 'B', 'keer' ]
+
+//shift
+let sharr = ['M', 'K', 'A', 'S', 'B']
+sharr.shift()
+console.log(sharr);//[ 'K', 'A', 'S', 'B' ]
+
+//unshift
+let unsharr = ['M', 'K', 'A', 'S', 'B']
+unsharr.unshift('Keer')
+console.log(unsharr);//[ 'Keer', 'M', 'K', 'A', 'S', 'B' ]
+
+//concat
+let con = ['add variable values']
+let con1 = ['here']
+let c = con.concat(con1)
+console.log(c);//[ 'add variable values', 'here' ]
+
+//join
+let jarr = ['madhu', 100, '@123']
+let j = jarr.join(' ')
+console.log(j);//madhu 100 @123
+
+//splice
+let splarr = ['madhu', 100, '@123']
+splarr.splice(0, 1, 'k', 'abc')
+console.log(splarr);//[ 'k', 'abc', 100, '@123' ]
+
+//slice
+let slarr = ['madhu', 100, '@123', 1, 2, 3, 4, 5]
+let a = slarr.slice(1, 5)
+console.log(a);//[ 100, '@123', 1, 2 ]
+
+//isarray
+let arr = ['madhu', 100, '@123', 1, 2, 3, 4, 5]
+let isarr = Array.isArray(arr)
+console.log(isarr)//true
+
+//indexof & lastindexof
+let indarr = [1, 2, 3, 6, 4, 5, 8, 10, 11, 89, 2, 6, 7]
+let inarr = indarr.indexOf(6)
+let inarray = indarr.lastIndexOf(6)
+console.log(inarr);//3
+console.log(inarray);//11
+
+//find
+let arrfind = [100, 20, 3, 4, 5, 6, 29, 13, 48, 59, 60, 25, 10, 15, 2, 9, 0, 35]
+function fun(no) {
+    return no >= 30//100
+    // return no<=10//it writes 3
+}
+let findno = arrfind.find(fun)
+console.log(findno);//100----this function writes first greater value it doesnot check the next values
+
+//findindex
+let arrfindin = [100, 20, 3, 4, 5, 6, 29, 13, 48, 59, 60, 25, 10, 15, 2, 9, 0, 35]
+function fun(no) {
+    return no >= 30
+}
+let findindex = arrfindin.findIndex(fun)
+console.log(findno);//100 --- it returns first index number
+
+//some
+let arrsome = [100, 20, 3, 4, 5, 6, 29, 13, 48, 59, 60, 25, 10, 15, 2, 9, 0, 35]
+let asome = arrsome.some(fun)
+function fun(no) {
+    return no >= 30
+}
+console.log(asome);
+
+//foreach--- needs function it takes three parameters value,index & array--executes the value each and every value
+let aryeach = [1, 7, 20, 18, 50]
+aryeach.forEach((item) => {
+    console.log("result: ", item * 10);
+})
+// result:  10
+// result:  70
+// result:  200
+// result:  180
+// result:  500
+
+// ex-2 foreach
+let areach = [1, 7, 20, 18, 50]
+areach.forEach(abc)
+
+function abc(item) {
+    console.log("result: ", item * 10);
+}
+// result:  10
+// result:  70
+// result:  200
+// result:  180
+// result:  500
+
+//filter
+let arrfil = [100, 20, 3, 4, 5, 6, 29, 13, 48, 59, 60, 25, 10, 15, 2, 9, 0, 35]
+function fun(no) {
+    return no >= 50
+}
+let filteritem = arrfil.filter(fun)
+console.log(filteritem);//[ 100, 59, 60 ]
+
+//every
+let arrevy = [100, 20, 3, 4, 5, 6, 29, 13, 48, 59, 60, 25, 10, 15, 2, 9, 0, 35]
+let aevy = arrevy.every(fun)
+function fun(no) {
+    return no >= 30
+}
+console.log(aevy);//false------ every item is greater than 30 then it returns true otherwise returns false
+
+//every ex-2
+let arrevyy = [31, 400, 50, 35, 60]
+let aevyy = arrevyy.every(fun)
+function fun(no) {
+    return no >= 30
+}
+console.log(aevyy);//true
+
+//include
+let incluarr = [1, 2, 4, 6, 7, 8, 'm', 'k', 's', 'b']
+let incarr = incluarr.includes('k')
+console.log(incarr);//true
+
+
+//fill
+let fiarr = [1, 2, 4, 6, 7, 8, 'm', 'k', 's', 'b']
+let fillarr = fiarr.fill('madhu')
+console.log(fillarr);//['madhu', 'madhu', 'madhu', 'madhu','madhu', 'madhu','madhu', 'madhu','madhu', 'madhu']
+
+//tostring
+let strarr = [1, 2, 4, 6, 7, 8, 'm', 'k', 's', 'b']
+let starr = strarr.toString()
+console.log(starr);//1,2,4,6,7,8,m,k,s,b
+
+//map
+let marr = [1, 2, 3, 4, 5, 7, 6]
+function fun(x) {
+    return x * 10
+}
+let maparray = marr.map(fun)
+console.log(maparray);//[10, 20, 30, 40, 50, 70, 60 ]
+
+//flat
+let a1 = [1, [2, 3, [4, 5, 6, [8, 9], 10], 11, 12], 13]
+let b = a1.flat(Infinity)//means all brackets remove
+let bc = a1.flat(3)//3 means 3 brackets remove i mention 1 1 bracket remove
+console.log(b);//[1,  2, 3,  4,  5, 6,  8, 9, 10, 11,  12, 13]
+console.log(bc);//[1,  2, 3,  4,  5, 6,  8, 9, 10, 11,  12, 13]
+
---
+++ task.js
@@ -0,0 +1,81 @@
+// task 1
+// find adult people mean age >=18
+
+let user = [
+    {name:"A",age:20},
+    {name:"B",age:18},
+    {name:"C",age:23},
+    {name:"D",age:28},
+    {name:"E",age:16},
+    {name:"F",age:19},
+    {name:"G",age:15},
+    {name:"H",age:21},
+    {name:"I",age:22}
+]
+let useritem=user.filter(((item)=>{
+    return item.age>=18
+}))
+console.log(useritem);
+//--------
+// user[0].totalmarks=100
+// console.log(user);
+
+//task2 question is add totalmarks
+
+user.forEach((item)=>{
+    item.totalmarks=100
+})
+console.log(user);
+
+//task3
+//find the no > 10 & even no also--
+let num=[23,12,9,6,17,1,5,3,2,22,29,1,9,20]
+let n=num.filter((item)=>{
+  return  item>=10 && item%2==0
+})
+console.log(n);//[ 12, 22, 20 ]
+
+//task4
+//create array 
+let people=['madhu','kk','keer','kadapa','sweets','frontend developer']
+let p=people.filter((item)=>{
+  return  item.length>4
+})
+console.log(p);//[ 'madhu', 'kadapa', 'sweets', 'frontend developer' ]
+
+//task5
+let user3=[
+    {name:"W",age:20},
+    {name:"X",age:18},
+    {name:"Y",age:23},
+    {name:"Z",age:28}
+]
+// let add=user.concat(user3)//1st method
+// console.log(add);
+let add1=[...user,...user3]//2nd method
+console.log(add1);
+
+//task5
+//find average age--
+
+let totaPeople=user.length
+console.log(totaPeople);//9
+
+let totaAge=user.reduce((a,c,index)=>{
+    return a+c.age
+},0)
+console.log(totaAge);//182
+
+let avgage=totaAge/totaPeople
+console.log(avgage);//20.22222222222222
+
+
+
+
+
+
+
+
+
+
+
