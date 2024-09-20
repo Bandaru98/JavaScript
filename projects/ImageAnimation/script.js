6ebe5f8fffaef47dffd48636b40b3e001c0b08ca
Bandaru98 <bandarumadhavi681998@gmail.com>
2024-09-20 17:06:07 +0530

    imageanimation

---
+++ projects/ImageAnimation/script.js
@@ -0,0 +1,16 @@
+const page = document.querySelector('body')
+page.addEventListener('mousemove', (event) => {
+    let x = event.offsetX
+    let y = event.offsetY
+    let span = document.createElement('span')
+    span.style.left = x + 'px'
+    span.style.top = y + 'px'
+    let size = Math.random() * 100
+    span.style.width = size + 'px'
+    span.style.height = size + 'px'
+    page.appendChild(span)
+    setTimeout(() => {
+        span.remove()
+    }, 3000)
+})
+
