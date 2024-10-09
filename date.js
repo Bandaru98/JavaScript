91bfe46f868587848e47d8cecac14d1f7f851632
Bandaru98 <bandarumadhavi681998@gmail.com>
2024-10-09 20:45:23 +0530

    update

--- date.js
+++ date.js
@@ -1,5 +1,5 @@
 //date
-let now=new Date()
+let now = new Date()
 console.log(now);//2024-09-11T10:41:59.083Z
 console.log(now.toString());//Wed Sep 11 2024 16:12:28 GMT+0530 (India Standard Time)
 console.log(now.getDate());//11
@@ -8,5 +8,20 @@ console.log(now.getFullYear());//2024
 console.log(now.toDateString());//Wed Sep 11 2024
 
 
+// [get] //
+function formate() {
+    let hours = now.getHours();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    return `${hours} ${ampm}`
+}
+console.log(formate());
+
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
 
 
+// [set] //
+
+let date = console.log(now.setFullYear(2022));
